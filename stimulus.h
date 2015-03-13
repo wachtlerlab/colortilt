844f60c59d40c55810ea3f138b605d3352f89f6c
Christian Kellner <christian@kellner.me>
2015-03-13 10:12:57 +0100

    stimulus: basic stimulus configuration

---
+++ stimulus.h
@@ -0,0 +1,24 @@
+#ifndef CT_STIMULUS_H
+#define CT_STIMULUS_H
+
+#include <rgb.h>
+#include <dkl.h>
+
+namespace colortilt {
+
+
+struct stimulus {
+
+    stimulus() { }
+    stimulus(double fg, double bg, float size) : phi_fg(fg), phi_bg(bg), size(size){ }
+
+    double phi_fg;
+    double phi_bg;
+
+    float size;
+};
+
+
+}
+
+#endif
