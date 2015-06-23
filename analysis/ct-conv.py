bae503d3c3e353f9372901cf3a3b60d14711d0a2
Christian Kellner <christian@kellner.me>
2015-06-23 11:23:11 +0200

    [analysis] old data -> new data

---
+++ analysis/ct-conv.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from __future__ import division
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import yaml
+import sys, os
+import fnmatch
+from datetime import date
+import time
+
+plt.style.use('ggplot')
+
+def main():
+    allfiles = os.listdir(sys.argv[1])
+    subject = sys.argv[2]
+    csvfiles = filter(lambda x: fnmatch.fnmatch(x, 'ci.%s.*.csv' % subject), allfiles)
+    print(csvfiles)
+
+    for f in csvfiles:
+        df = pd.read_csv(f, skipinitialspace=True)
+        df['bg'] = np.round(df['bg'] / np.pi * 180.0, 2)
+        df['fg'] = np.round(df['fg'] / np.pi * 180.0, 2)
+        df['phi'] = df['match'] / np.pi * 180.0
+        df['phi_start'] = np.nan
+        df['duration'] = np.nan
+        del df['match']
+        st = os.stat(f)
+        tstr = time.strftime("%Y%m%dT%H%M", time.gmtime(st.st_ctime))
+        bgs = np.unique(df['bg'])
+        bg_id = 'on' if 0 in bgs else 'off'
+        sizes = '+'.join(map(str, map(int, sorted(np.unique(df['size'])))))
+        sides = ''.join(sorted(np.unique(df['side'])))
+        newfn = '%s_%s_off_%s_%s.dat' % (tstr, bg_id, sizes, sides)
+        df.to_csv(newfn, sep=',', encoding='utf-8')
+        print(newfn)
+
+
+if __name__ == "__main__":
+    main()
