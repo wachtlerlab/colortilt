6cbe887c03c10c6ca1725cde6a9a088c5fd9c471
Christian Kellner <christian@kellner.me>
2015-06-26 15:23:48 +0200

    [analysis] sizerel: cmd param for mean vs min/max

--- analysis/ct-sizerel.py
+++ analysis/ct-sizerel.py
@@ -18,22 +18,29 @@ def read_data(file_list):
             df = df.append(to_append, ignore_index=True)
     return df
 
-def mean_angle(df, sign):
+def mean_angle(df, sign, use_mean=False):
     x = df[df.fg == sign*22.5].append(df[df.fg == sign*67.5], ignore_index=True)
     grouped = x.groupby(['bg', 'size'])
-    df_mean = grouped['shift'].agg({'m_plus' if sign > 0 else 'm_minus': np.mean})
+
+    if use_mean:
+        fn = np.mean
+    else:
+        fn = np.max if sign > 0 else np.min
+
+    df_mean = grouped['shift'].agg({'m_plus' if sign > 0 else 'm_minus': fn})
     return df_mean
 
 def main():
     parser = argparse.ArgumentParser(description='CT - Analysis')
     parser.add_argument('data', nargs='+', type=str, default=['-'])
+    parser.add_argument('--mean', type=bool, default=False)
     args = parser.parse_args()
 
     df = read_data(args.data)
     df = df[df.bg != -1]
-    m_plus = mean_angle(df, 1)
+    m_plus = mean_angle(df, 1, use_mean=args.mean)
     print(m_plus, file=sys.stderr)
-    m_minus = mean_angle(df, -1)
+    m_minus = mean_angle(df, -1, use_mean=args.mean)
     print(m_minus, file=sys.stderr)
 
     x = pd.concat([m_plus, m_minus], axis=1)
