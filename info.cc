deca2e9863276d3d8bc3d4ae3abdfd988ed06c40
Christian Kellner <christian@kellner.me>
2015-07-24 18:14:52 +0200

    [info] print out overview of done vs todo sessions

--- info.cc
+++ info.cc
@@ -2,14 +2,71 @@
 #include "exp.h"
 
 #include <stdexcept>
+#include <set>
+
+struct session_info {
+
+    std::string name;
+
+    std::vector<std::string> todo;
+    std::vector<std::string> done;
+};
+
+struct session_set {
+
+    session_info& add(const colortilt::session &info);
+    session_info& add(const colortilt::trail &trail);
+    std::vector<session_info> sessions;
+};
+
+
+session_info &session_set::add(const colortilt::session &info) {
+
+    for (session_info &i : sessions) {
+        if (i.name == info.stim) {
+            i.todo.push_back(info.rnd);
+            return i;
+        }
+    }
+
+    session_info nfo;
+    nfo.name = info.stim;
+    nfo.todo = std::vector<std::string>{info.rnd};
+    sessions.push_back(nfo);
+    return sessions.back();
+}
+
+session_info &session_set::add(const colortilt::trail &trail) {
+
+    for (session_info &i : sessions) {
+        if (i.name == trail.stim) {
+            i.done.push_back(trail.rnd);
+            return i;
+        }
+    }
+
+    session_info nfo;
+    nfo.name = trail.stim;
+    nfo.done = std::vector<std::string>{trail.rnd};
+    sessions.push_back(nfo);
+    return sessions.back();
+}
+
+
+
 
 int main(int argc, char **argv) {
 
     try {
         iris::data::store store = iris::data::store::default_store();
 
+        std::string baseloc = "";
+        if (argc > 1) {
+            baseloc = argv[1];
+        }
+
         // load the experiment data
-        fs::file exp_file = colortilt::experiment::find_file("");
+        fs::file exp_file = colortilt::experiment::find_file(baseloc);
         if (exp_file.path().empty()) {
             std::cerr << "Could not find experiment file!" << std::endl;
             return -1;
@@ -47,17 +104,42 @@ int main(int argc, char **argv) {
             std::cout << std::endl;
         }
 
-        std::cout << std::endl << "Sessions: " << std::endl;
+        std::cout << std::endl << "Subjects: " << std::endl;
+
         for (const iris::data::subject &subject : subjects) {
             std::vector<colortilt::session> sessions = exp.load_sessions(subject);
+            session_set unique;
 
             std::cout << "  " << subject.name << std::endl;
             for (const colortilt::session &s : sessions) {
-                std::cout << "    " << s.name() << std::endl;
+                unique.add(s);
+
             }
 
-        }
+            std::vector<colortilt::trail> trails = exp.list_trails(subject);
+            for (const colortilt::trail &trail : trails) {
+                unique.add(trail);
+            }
 
+            int todo = 0;
+            int done = 0;
+            long diff = 0;
+            for (const session_info &i : unique.sessions) {
+                std::cout << "      " << i.name << "\t" << i.done.size();
+                std::cout << " / " << i.todo.size() << std::endl;
+
+                todo += i.todo.size();
+                done += i.done.size();
+                long d = i.todo.size() - i.done.size();
+
+                if (d > 0) {
+                    diff += d;
+                }
+            }
+
+            std::cout << "    * Summary: \t " << done << " / " << todo;
+            std::cout << " [" << diff << "]" << std::endl;
+        }
 
     } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
