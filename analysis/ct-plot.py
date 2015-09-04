36c8182d1ade16d26ae1967bb6c841d24e8362d4
Christian Kellner <christian@kellner.me>
2015-09-04 15:39:27 +0200

    [analysis] move some plotting code to plot module

--- analysis/ct-plot.py
+++ analysis/ct-plot.py
@@ -13,14 +13,7 @@ from matplotlib.colors import rgb_to_hsv, hsv_to_rgb
 from utils import ggsave
 
 from colortilt.io import read_data
-
-
-def mk_rgb(value):
-    if type(value) == str and len(value) == 7 and value[0] == '#':
-        import struct
-        t = map(lambda x: x/255.0, struct.unpack('BBB', value[1:].decode('hex')))
-        return list(t + [1.0, ])
-    raise ValueError('Invalid input')
+from colortilt.plot import (angles_to_color, mk_rgb)
 
 
 def make_idx2pos():
@@ -140,25 +133,6 @@ def plot_shifts(df, cargs):
                             fontsize=18, family='Input Mono', color=angles_to_color([bg])[0])
 
     return figures
-
-
-def angles_to_color(angles):
-    def rgb(r, g, b):
-        return "#%02x%02x%02x" % (r, g, b)
-
-    bg = list(np.arange(0, 360, 360.0/8))
-
-    cls = [rgb(252, 38, 28),
-           rgb(253, 77, 252),
-           rgb(13, 66, 251),
-           rgb(49, 183, 229),
-           rgb(26, 176, 29),
-           rgb(181, 248, 50),
-           rgb(216, 202, 43),
-           rgb(252, 151, 39),
-           rgb(0, 0, 0)  #fallback color
-           ]
-    return [cls[bg.index(a) if a in bg else 8] for a in angles]
 
 
 def plot_delta(df, cargs):
