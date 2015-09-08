69f3907ed1162a6549a732cd237c49aa9091302d
Christian Kellner <christian@kellner.me>
2015-09-08 18:27:02 +0200

    [analysis] ct-scat: use mean we have proepr values

--- analysis/ct-scat.py
+++ analysis/ct-scat.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 from __future__ import division
 
 import pandas as pd
+import numpy as np
 import argparse
 import sys
 
@@ -10,15 +11,16 @@ from colortilt.io import read_data
 
 
 def calc_delta(a):
-    x_10 = a.loc[a['size'] == 10.0]['shift'].values[0]
-    x_40 = a.loc[a['size'] == 40.0]['shift'].values[0]
-    x_160 = a.loc[a['size'] == 160.0]['shift'].values[0]
+    x_10 = np.mean(a.loc[a['size'] == 10.0]['shift'].values)
+    x_40 = np.mean(a.loc[a['size'] == 40.0]['shift'].values)
+    x_160 = np.mean(a.loc[a['size'] == 160.0]['shift'].values)
     fg = a['fg'].unique()
     assert(len(fg) == 1)
     sign = -1 if fg < 0 else 1
     return pd.Series({'40': sign * x_40,
                       'delta': sign * (x_10 - x_160),
                       'sign': sign})
+
 
 def main():
     parser = argparse.ArgumentParser(description='CT - Analysis')
