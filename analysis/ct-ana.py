caea1d6292f9adada222224fb940f27af5a76a12
Christian Kellner <christian@kellner.me>
2015-09-08 18:46:46 +0200

    [analysis] remove redundant loading code

--- analysis/ct-ana.py
+++ analysis/ct-ana.py
@@ -10,16 +10,6 @@ import sys, os
 from colortilt.io import read_data
 
 
-def calc_angle_shift(phi, baseline, input_is_radiants=False):
-    if input_is_radiants:
-        phi = phi/np.pi*180.0
-        baseline = baseline/np.pi*180.0
-    shift = float(phi - baseline)
-    shift += (shift >  180.0) * -360
-    shift += (shift < -180.0) *  360
-    return shift
-
-
 def stdnerr(x):
     return np.std(x)/np.sqrt(len(x))
 
@@ -32,17 +22,14 @@ def main():
 
     df = read_data([args.data])
 
-    df['shift'] = df['phi'].combine(df['fg'], calc_angle_shift)
-    df['fg_rel'] = df['fg'].combine(df['bg'], calc_angle_shift)
+    groups = ['bg', 'size', 'fg', 'subject']
+    columns = ['bg', 'size', 'fg', 'subject', 'shift', 'err', 'N']
 
     if args.combine:
-        groups = ['bg', 'size', 'fg_rel']
-        columns = ['bg', 'size', 'fg', 'shift', 'err', 'N']
-    else:
-        groups = ['bg', 'size', 'fg_rel', 'subject']
-        columns = ['bg', 'size', 'fg', 'subject', 'shift', 'err', 'N']
+        groups.remove('subject')
+        columns.remove('subject')
 
-    gpd = df[['bg', 'fg_rel', 'size', 'shift', 'subject']].groupby(groups, as_index=False)
+    gpd = df[['bg', 'fg', 'size', 'shift', 'subject']].groupby(groups, as_index=False)
     dfg = gpd.agg([np.mean, stdnerr, len])
     x = dfg.reset_index()
     x.columns = columns
