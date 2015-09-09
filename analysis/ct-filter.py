4b5c6ea77b34677dc3d09754e3e76627f4897963
Christian Kellner <christian@kellner.me>
2015-09-09 18:24:06 +0200

    [analysis] ct-filter exclude control

--- analysis/ct-filter.py
+++ analysis/ct-filter.py
@@ -12,12 +12,16 @@ def main():
     parser = argparse.ArgumentParser(description='CT analysis - Filter')
     parser.add_argument('data', nargs='+', type=str, default=['-'])
     parser.add_argument('--size', default=None)
+    parser.add_argument('--no-control', action='store_true', default=False, dest='ctrl')
     args = parser.parse_args()
 
     df = read_data(args.data)
 
     if args.size is not None:
         df = df[df.size == int(args.size)]
+
+    if args.ctrl:
+        df = df[df.bg != -1]
 
     df.to_csv(sys.stdout, index=False)
 
