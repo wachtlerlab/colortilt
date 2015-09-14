899b1c60fd532703ea47c92ea93ff9d1929617f0
Christian Kellner <christian@kellner.me>
2015-09-14 15:03:45 +0200

    [analysis] make_plots update

--- analysis/make_plots.sh
+++ analysis/make_plots.sh
@@ -11,17 +11,22 @@ mkdir "$DIR"
 #func do all analysis
 
 make_shift_plots() {
-    ./ct-load.py "$EXPFILE" "$1" | ./ct-ana.py --combine - | ./ct-plot.py -S -P "$DIR" -W 60 -H 40 --ylim 35 - 2>> log.tmp
-    ./ct-load.py "$EXPFILE" "$1" | ./ct-ana.py --combine --col=duration  - | ./ct-plot.py -S -P "$DIR" -W 60 -H 40 - 2>> log.tmp
+    ./ct-load.py "$EXPFILE" "$1" | ./ct-ana.py -C | ./ct-plot.py -S -P "$DIR" -W 60 -H 40 --ylim 35 2>> log.tmp
+    ./ct-load.py "$EXPFILE" "$1" | ./ct-ana.py -C --col=duration | ./ct-plot.py -S -P "$DIR" -W 60 -H 40 2>> log.tmp
 
-    ./ct-load.py "$EXPFILE" "$1" |  ./ct-filter.py --no-control - | ./ct-ana.py --combine - | ./ct-conv.py rel2abs | ./ct-plot.py --vertical -S -P "$DIR" -W 20 -H 60 --ylim 35 - 2>> log.tmp
+    ./ct-load.py "$EXPFILE" "$1" |  ./ct-filter.py --no-control | ./ct-ana.py -C | ./ct-conv.py rel2abs | ./ct-plot.py --vertical -S -P "$DIR" -W 20 -H 60 --ylim 35 2>> log.tmp
+
+    ./ct-load.py "$EXPFILE" "$1" | ./ct-conv.py rel2abs | ./ct-ana.py -C -M | ./ct-plot.py -S -P "$DIR" 2>> log.tmp
+    ./ct-load.py "$EXPFILE" "$1" | ./ct-ana.py -C -M | ./ct-plot.py -S -P "$DIR" 2>> log.tmp
+
+    ./ct-load.py "$EXPFILE" "$1" | ./ct-filter.py --no-control | ./ct-conv.py rel2abs | ./ct-ana.py -C | ./ct-spread.py | ./ct-plot.py --vertical -W 20 -H 60 -S -P "$DIR" 2>> log.tmp
 }
 
 
 # read in subjects
 while IFS= read -r; do
     subjects+=("$REPLY")
-done < <(./ct-load.py ../../colortilt-data/experiment | ./ct-ana.py - | ./ct-N.py --subjects -)
+done < <(./ct-load.py ../../colortilt-data/experiment | ./ct-ana.py | ./ct-N.py --subjects)
 [[ $REPLY ]] && subjects+=("$REPLY")
 
 
@@ -34,13 +39,20 @@ echo "[p] shift *"
 make_shift_plots
 
 echo "[p] sizerel"
-./ct-load.py "$EXPFILE" | ./ct-ana.py - | ./ct-sizerel.py - | ./ct-plot.py -S -P "$DIR" -W 20 -H 32 - 2>> log.tmp
+./ct-load.py "$EXPFILE" | ./ct-ana.py | ./ct-sizerel.py | ./ct-plot.py -S -P "$DIR" -W 20 -H 32 2>> log.tmp
+./ct-load.py "$EXPFILE" | ./ct-filter.py --fg-sign='-' | ./ct-ana.py | ./ct-sizerel.py | ./ct-plot.py -F 'sizerel-pos.pdf' -S -P "$DIR" -W 20 -H 32 2>> log.tmp
+./ct-load.py "$EXPFILE" | ./ct-filter.py --fg-sign='+' | ./ct-ana.py | ./ct-sizerel.py | ./ct-plot.py -S 'sizerel-neg.pdf' -P "$DIR" -W 20 -H 32 2>> log.tmp
+
+echo "[p] spread-sizrel:"
+./ct-load.py "$EXPFILE" | ./ct-filter.py --no-control | ./ct-ana.py -C | ./ct-spread.py --sizerel | ./ct-plot.py -F "sizerel-spread.pdf" -S -P "$DIR" -W 20 -H 32 2>> log.tmp
 
 echo "[p] scat"
-./ct-load.py "$EXPFILE" | ./ct-ana.py --combine - | ./ct-scat.py - | ./ct-plot.py -S -P "$DIR" -W 32 -H 20 - 2>> log.tmp
+./ct-load.py "$EXPFILE" | ./ct-ana.py -C | ./ct-scat.py | ./ct-plot.py -S -P "$DIR" -W 32 -H 20 2>> log.tmp
 
 echo "[p] old vs. new"
-./ct-load.py "$EXPFILE" twachtler | ./ct-ana.py - | ./ct-cmpold.py --inner twachtler allmatchesTW.csv - | ./ct-plot.py -S -P "$DIR" -W 60 -H 40 --ylim 35 - 2>> log.tmp
+./ct-load.py "$EXPFILE" twachtler | ./ct-ana.py | ./ct-cmpold.py --inner twachtler allmatchesTW.csv | ./ct-plot.py -S -P "$DIR" -W 60 -H 40 --ylim 35 2>> log.tmp
 
+
+echo "Plots:"
 ls "$DIR"
 echo "All done."
