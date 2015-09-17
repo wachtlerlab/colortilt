2c5959b129d8081b941bb7d05ec885fcba8d773f
Christian Kellner <christian@kellner.me>
2015-09-17 19:11:02 +0200

    [analysis] filter: add bg filter, pep8 fixes

--- analysis/ct-filter.py
+++ analysis/ct-filter.py
@@ -7,20 +7,29 @@ import sys
 
 from colortilt.io import read_data
 
+
 def filter_control(df):
     return df[df.bg != -1]
+
 
 def filter_size(df, size):
     df = df[df.size == int(size)]
     return df
 
+
 def filter_fg_range(df, fg):
     return df[(-fg <= df.fg) & (df.fg <= +fg)]
+
 
 def filter_fg_sign(df, sign):
     if sign not in ['+', '-']:
         raise ValueError("Sign must be + or -")
     return df[(df.fg > 0 if sign == '+' else df.fg < 0)]
+
+
+def filter_bg(df, bg):
+    return df[df.bg == float(bg)]
+
 
 def main():
     parser = argparse.ArgumentParser(description='CT analysis - Filter')
@@ -28,6 +37,7 @@ def main():
     parser.add_argument('--size', default=None)
     parser.add_argument('--no-control', action='store_true', default=False, dest='ctrl')
     parser.add_argument('--fg', dest='fg', type=float, default=None)
+    parser.add_argument('-B', '--bg', dest='bg', type=float, default=None)
     parser.add_argument('--fg-sign', dest='fg_sign', default=None)
     args = parser.parse_args()
 
@@ -44,6 +54,10 @@ def main():
 
     if args.fg_sign:
         df = filter_fg_sign(df, args.fg_sign)
+
+    if args.bg is not None:
+        df = filter_bg(df, args.bg)
+
 
     df.to_csv(sys.stdout, index=False)
 
