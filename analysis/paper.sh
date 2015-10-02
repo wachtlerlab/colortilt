a05156a43b729d4b87ee54d6e95c574fca2b7774
Christian Kellner <christian@kellner.me>
2015-10-02 17:47:58 +0200

    [analysis] paper.sh: fig 5 changes

--- analysis/paper.sh
+++ analysis/paper.sh
@@ -18,7 +18,9 @@ echo "[p] Figure 4"
 ./ct-load.py "$EXPFILE" | ./ct-filter.py --no-control | ./ct-ana.py -C | ./ct-conv.py abs-shift | ./ct-spread.py --sizerel | ./ct-plot.py -S -F sizerel.pdf -P "$DIR" -U in -W 5.75 -H 7 2>> log.tmp
 
 echo "[p] Figure 5"
-./ct-load.py "$EXPFILE" | ./ct-filter.py --no-control | ./ct-ana.py -C | ./ct-conv.py abs-shift | ./ct-spread.py --max-spread | ./ct-plot.py --ylim 25 -S -F "maxspread.pdf" -P "$DIR" -U in -W 5.75 -H 5.75 2>> log.tmp
+./ct-load.py "$EXPFILE" | ./ct-filter.py --no-control | ./ct-ana.py -C | ./ct-conv.py abs-shift | ./ct-spread.py --sizerel | ./ct-slope.py --method mean size | ./ct-plot.py -S -F "slope28.pdf" -P "$DIR" -U in -W 5.75 -H 5.75 2>> log.tmp
+./ct-load.py "$EXPFILE" | ./ct-filter.py --no-control | ./ct-ana.py -C | ./ct-conv.py abs-shift | ./ct-spread.py --sl-rel 40 | ./ct-slope.py --method mean size | ./ct-plot.py -S -F "slope28rel.pdf" -P "$DIR" -U in -W 5.75 -H 5.75 2>> log.tmp
+
 
 echo "[p] Figure 6"
 ./ct-load.py "$EXPFILE" twachtler | ./ct-ana.py | ./ct-cmpold.py --inner twachtler allmatchesTW.csv | ./ct-filter.py -B 0.0 | ./ct-plot.py --ylim 35 --single --no-title -S -P "$DIR" -F twctcmp.pdf -W 16 -H 12 2>> log.tmp
