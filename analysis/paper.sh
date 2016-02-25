2c8124c2dc20c2ea2b276421a3fcea5da83cf1c8
Christian Kellner <christian@kellner.me>
2016-02-25 18:45:50 +0100

    paper.sh: don't do figure 6, no included in paper

--- analysis/paper.sh
+++ analysis/paper.sh
@@ -19,7 +19,3 @@ echo "[p] Figure 4"
 echo "[p] Figure 5"
 ./ct-load.py "$EXPFILE" | ./ct-filter.py --no-control | ./ct-ana.py -C | ./ct-conv.py abs-shift | ./ct-spread.py --sizerel | ./ct-slope.py --method mean size | ./ct-plot.py --no-legend -S -F "slope28.eps" -P "$DIR" -U in -W 5.75 -H 5.75 --scale 0.5 --daylight 2>> log.tmp
 ./ct-load.py "$EXPFILE" | ./ct-filter.py --no-control | ./ct-ana.py -C | ./ct-conv.py abs-shift | ./ct-spread.py --sl-rel 40 | ./ct-slope.py --method mean size | ./ct-plot.py --no-legend -S -F "slope28rel.eps" -P "$DIR" -U in -W 5.75 -H 5.75 --scale 0.5 2>> log.tmp
-
-
-echo "[p] Figure 6"
-./ct-load.py "$EXPFILE" twachtler | ./ct-ana.py | ./ct-cmpold.py --inner twachtler allmatchesTW.csv | ./ct-filter.py -B 0.0 | ./ct-plot.py --ylim 35 --single --no-title -S -P "$DIR" -F twctcmp.eps -W 16 -H 12 2>> log.tmp
